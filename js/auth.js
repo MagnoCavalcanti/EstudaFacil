faa49a2d0c89a0887cb6f45d599a792fab006b83
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-06-13 17:53:37 -0300

    colocando um auth

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,17 @@
 let form = document.querySelector('form');
 
-        form.onsubmit = function(event) {
-            event.preventDefault();
-            
-            window.location.href = "/main.html";
-        }
-    
+form.onsubmit = function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('senha').value;
+
+    if (email === 'teste@teste.com' && password === 'teste') {
+        window.location.href = "/planner.html";
+    } else if (email == "" || password == "") {
+        alert('Preencha todos os campos');
+    } else {
+        alert('Email ou senha inválidos');
+    }
+
+    // window.location.href = "/planner.html";
+}
